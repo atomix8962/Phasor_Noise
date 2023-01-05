c25844e0b83f346b4bd73e35245a054c56e20770
ETN64 <36243927+ETN64@users.noreply.github.com>
2023-01-05 19:31:56 +0000

    Add an image reader stored in image_reader.py Add a Noise used for test Noise.png Update medium.py

---
+++ analysis/image_reader.py
@@ -0,0 +1,15 @@
+from PIL import Image,ImageTk
+import math
+
+"""
+Load an image using his path and return the value of the shade of black for each pixel (0 to 255)
+Where the data are stored using data[y_coordinate][x_coordinate]
+"""
+
+def getImageData(path:str)->list:
+    img=Image.open(path)
+    rawData=img.load()
+    data=[]
+    for y in range(img.height):
+        data.append([rawData[x,y][0] for x in range(img.width)])
+    return data
--- analysis/medium.py
+++ analysis/medium.py
@@ -1,7 +1,17 @@
+import image_reader as ir
 '''
 medium.py
 --> Réalisation d'une moyenne des valeurs données dans un tableau 
 '''
 
 def medium(noiseArray:list) -> float:
-    pass
+    sum=0
+    n=0
+    for y in range(len(noiseArray)):
+        for x in range(len(noiseArray[y])):
+            sum+=noiseArray[y][x]
+            n+=1
+    return sum/n
+
+
+print(medium(ir.getImageData("Noise.png")))
