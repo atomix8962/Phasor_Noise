05e7be0e65c138cc8f10562ecdd050b832638295
ETN64 <36243927+ETN64@users.noreply.github.com>
2023-01-05 21:10:45 +0000

    Power Spectral Density using the Fourier Transform

--- analysis/PSD.py
+++ analysis/PSD.py
@@ -1,7 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
 '''
 PSD.py
---> (Noter le but de la/le fonction/module)
+--> The Power Spectral Density PSD of a bi-dimensional signal using the DFT
+Input : A numpy array of dimension NxM
 '''
-
-def PSD() -> None:
-    pass
+def PSD_2D(signalArray:np.array) -> np.array:
+    n_x,n_y=signalArray.shape
+    psd=np.zeros((n_x,n_y))
+    for y in range(n_y):
+        #Applying the Discrete Fourier Transform
+        dft=np.fft.fft(signalArray[:,y])
+        dft_amp=np.abs(dft)
+        psd[:,y]=dft_amp**2/(2*np.pi)
+    return psd
