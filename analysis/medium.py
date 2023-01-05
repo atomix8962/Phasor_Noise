95b4f87a90f03c162f728440f094c12a39fcc49c
ETN64 <36243927+ETN64@users.noreply.github.com>
2023-01-05 20:44:26 +0100

    Rename mean.py

--- analysis/medium.py
+++ analysis/medium.py
@@ -1,17 +1,8 @@
 import image_reader as ir
 '''
-medium.py
+mean.py
 --> Réalisation d'une moyenne des valeurs données dans un tableau 
 '''
 
-def medium(noiseArray:list) -> float:
-    sum=0
-    n=0
-    for y in range(len(noiseArray)):
-        for x in range(len(noiseArray[y])):
-            sum+=noiseArray[y][x]
-            n+=1
-    return sum/n
-
-
-print(medium(ir.getImageData("Noise.png")))
+def mean(noiseArray:list) -> float:
+    
