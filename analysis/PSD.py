0aa15534808632b0f0a4275aa015d217940c0047
ETN64 <36243927+ETN64@users.noreply.github.com>
2023-01-06 08:52:14 +0000

    Updated 06/01

--- analysis/PSD.py
+++ analysis/PSD.py
@@ -2,15 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 '''
 PSD.py
---> The Power Spectral Density PSD of a bi-dimensional signal using the DFT
-Input : A numpy array of dimension NxM
+--> The Power Spectral Density PSD is defined by :
+P = |x̂(f)|² with x̂ the Fourier Transform of the Signal
 '''
-def PSD_2D(signalArray:np.array) -> np.array:
-    n_x,n_y=signalArray.shape
-    psd=np.zeros((n_x,n_y))
-    for y in range(n_y):
-        #Applying the Discrete Fourier Transform
-        dft=np.fft.fft(signalArray[:,y])
-        dft_amp=np.abs(dft)
-        psd[:,y]=dft_amp**2/(2*np.pi)
+def PSD(signal):
+    signal_hat=np.fft.fft(signal)
+    psd=np.abs(signal_hat)**2
     return psd
+
+def testSignal(x,y):
+    f1=1
+    f2=2
+    return np.sin(f1*2*np.pi*x)+np.sin(f2*2*np.pi*y)
+
+X,Y=np.arange(0,5,0.01),np.arange(0,10,0.02)
+signal=testSignal(X,Y)
+psd=PSD(signal)
+fig, (ax1,ax2,ax3)=plt.subplots(3,1)
+ax1.plot(X,signal)
+ax2.plot(Y,signal)
+ax3.plot(X,psd)
+plt.show()
