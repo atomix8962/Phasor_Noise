80ada218d4eda1adb0e4736d44b2484c3328e87e
Pmrt88 <121969053+Pmrt88@users.noreply.github.com>
2023-01-26 15:08:05 +0100

    Add files via upload
    moyenne et écart-type d'un image en format RGBA ou P

---
+++ analysis/image_analyse_PaulM.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import numpy as np
+
+image = Image.open("./data/phasornoise_capture.png").convert('RGBA')
+image_numpy = np.array(image)
+image_numpy_R = image_numpy[:,:,0].astype(np.float64)/255
+image_numpy_R_flattened = image_numpy_R.reshape((-1,))
+
+
+# calculer moyenne
+def mean(noiseArray:list)-> float:
+    return np.average(noiseArray)
+
+print(mean(image_numpy_R_flattened))
+
+
+# calculer ecart-type
+def std_gap(noiseArray:list) -> float:
+    return np.std(noiseArray)
+
+print(std_gap(image_numpy_R_flattened))
